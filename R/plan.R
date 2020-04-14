fcb8528519bf2762c74697421926dcb942d04599
mihagazvoda <miha.gazvoda1@gmail.com>
2020-04-14 20:34:19 +0200

    update to scrape all goodread books

--- R/plan.R
+++ R/plan.R
@@ -1,5 +1,5 @@
 plan <- drake_plan(
-  books = map_dfr(c(1:1), get_books), # TODO should website url as argument
+  books = map_dfr(c(1:8), get_books), # TODO should website url as argument
   weighted_genres = summarise_genres(books),
   word_cloud = plot_word_cloud(weighted_genres)
 )
